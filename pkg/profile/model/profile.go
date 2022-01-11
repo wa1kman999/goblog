6ee8e5103e696a46dba7ea21eca69ce64cc21a05
徐静 <xujing@dustess.com>
2022-01-11 20:14:34 +0800

    feat:优化

--- pkg/profile/model/profile.go
+++ pkg/profile/model/profile.go
@@ -2,15 +2,15 @@ package model
 
 // Profile 个人信息
 type Profile struct {
-	ID        int    `cond:"primaryKey" json:"id"`
-	Name      string `cond:"type:varchar(20)" json:"name"`
-	Desc      string `cond:"type:varchar(200)" json:"desc"`
-	QQ        string `cond:"type:varchar(200)" json:"qq"`
-	Wechat    string `cond:"type:varchar(100)" json:"wechat"`
-	Weibo     string `cond:"type:varchar(200)" json:"weibo"`
-	Bili      string `cond:"type:varchar(200)" json:"bili"`
-	Email     string `cond:"type:varchar(200)" json:"email"`
-	Img       string `cond:"type:varchar(200)" json:"img"`
-	Avatar    string `cond:"type:varchar(200)" json:"avatar"`
-	IcpRecord string `cond:"type:varchar(200)" json:"icp_record"`
+	ID        int    `gorm:"primaryKey" json:"id"`
+	Name      string `gorm:"type:varchar(20)" json:"name"`
+	Desc      string `gorm:"type:varchar(200)" json:"desc"`
+	QQ        string `gorm:"type:varchar(200)" json:"qq"`
+	Wechat    string `gorm:"type:varchar(100)" json:"wechat"`
+	Weibo     string `gorm:"type:varchar(200)" json:"weibo"`
+	Bili      string `gorm:"type:varchar(200)" json:"bili"`
+	Email     string `gorm:"type:varchar(200)" json:"email"`
+	Img       string `gorm:"type:varchar(200)" json:"img"`
+	Avatar    string `gorm:"type:varchar(200)" json:"avatar"`
+	IcpRecord string `gorm:"type:varchar(200)" json:"icp_record"`
 }

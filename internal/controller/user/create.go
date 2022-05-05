e96ae6506682a2a47e35331aff33b78b03f5fe42
wa1kman <wa1kman@163.com>
2022-05-05 17:47:13 +0800

    feat: user list

--- internal/controller/user/create.go
+++ internal/controller/user/create.go
@@ -4,20 +4,29 @@ import (
 	"github.com/gin-gonic/gin"
 	userService "github.com/wa1kman999/goblog/internal/application/service/user"
 	"github.com/wa1kman999/goblog/internal/http/vs"
+	"github.com/wa1kman999/goblog/pkg/user/model"
+
 	//"github.com/sirupsen/logrus"
 	"github.com/wa1kman999/goblog/pkg/common/logger"
-	"github.com/wa1kman999/goblog/pkg/user/model"
 )
 
+type CreateUserReq struct {
+	UserName string `json:"userName" binding:"required"`
+	PassWord string `json:"passWord" binding:"required"`
+}
+
 func CreateUser(ctx *gin.Context) {
-	var user model.User
+	var r CreateUserReq
 	log := logger.WithContext(ctx.Request.Context())
-	if err := ctx.ShouldBindJSON(&user); err != nil {
+	if err := ctx.ShouldBindJSON(&r); err != nil {
 		log.Errorf(err, "user param is invalid")
 		vs.SendParamParseError(ctx)
 		return
 	}
-	if err := userService.NewAppFormService().CreateUser(&user); err != nil {
+	if err := userService.NewAppFormService().CreateUser(&model.User{
+		Username: r.UserName,
+		Password: r.PassWord,
+	}); err != nil {
 		log.Errorf(err, "create user failed")
 		vs.SendBad(ctx, err)
 		return
